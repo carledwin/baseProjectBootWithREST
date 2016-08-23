837185984d7114b492787ec6c8c22b00159dd090
carledwinj <carlinstr@gmail.com>
2016-08-23 15:04:21 -0300

    Initial

---
+++ src/main/resources/db/migration/V03__inserir_despesas.sql
@@ -0,0 +1,4 @@
+INSERT INTO db_baseprojectbootwithrest.despesa (id, name, description) VALUES (2, 'despesa 2', 'descrição despesa 2');
+INSERT INTO db_baseprojectbootwithrest.despesa (id, name, description) VALUES (3, 'despesa 3', 'descrição despesa 3');
+INSERT INTO db_baseprojectbootwithrest.despesa (id, name, description) VALUES (4, 'despesa 4', 'descrição despesa 4');
+INSERT INTO db_baseprojectbootwithrest.despesa (id, name, description) VALUES (5, 'despesa 5', 'descrição despesa 5');
